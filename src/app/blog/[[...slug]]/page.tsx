faa88e5767b2a94505f2ad984430ad5d865aadac
Atif ahmed <167696985+Atif1254@users.noreply.github.com>
2025-02-06 12:40:33 +0000

    Update page.tsx

--- src/app/blog/[[...slug]]/page.tsx
+++ src/app/blog/[[...slug]]/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { fullBlog } from "../../../sanity/interface";
 import SanityClient from "@/sanity/lib/client"
 import { urlFor } from "@/sanity/lib/image";
